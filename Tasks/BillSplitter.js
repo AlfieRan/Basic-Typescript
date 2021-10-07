34d1e3ff52ce992665cd1d93d16588a55c198553
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-10-07 10:23:21 +0100

    added rounding to 2dp

--- Tasks/BillSplitter.js
+++ Tasks/BillSplitter.js
@@ -8,8 +8,8 @@ function BillSplitter() {
     var bill = parseFloat(prompt("What is the total bill?"));
     var people = parseInt(prompt("How many people are sharing?"));
     var tip_percentage = parseInt(prompt("What percentage tip would you like to leave?"));
-    var TotalBill = (bill + (bill * (tip_percentage / 100)));
-    var cost_per_person = TotalBill / people;
+    var TotalBill = Math.round(100 * (bill + (bill * (tip_percentage / 100)))) / 100;
+    var cost_per_person = Math.round(TotalBill * 100 / people) / 100;
     console.log("\nTotal bill including tip is " + TotalBill + "\nTotal cost per person is " + cost_per_person);
 }
 exports.BillSplitter = BillSplitter;
--- Tasks/BillSplitter.ts
+++ Tasks/BillSplitter.ts
@@ -3,10 +3,10 @@ const prompt = promptSync();
 export function BillSplitter()
 {
     console.log("---Welcome to Split My Bill---");
-    let bill:number = parseFloat(prompt("What is the total bill?"));
+    let bill:number = parseFloat(prompt("What is the total bill?")); //Tried putting the console.log line above in this prompt but it did not like it
     let people:number = parseInt(prompt("How many people are sharing?"));
     let tip_percentage:number = parseInt(prompt("What percentage tip would you like to leave?"));
-    let TotalBill:number = (bill + (bill * (tip_percentage / 100)));
-    let cost_per_person:number = TotalBill / people;
+    let TotalBill:number = Math.round(100*(bill + (bill * (tip_percentage / 100))))/100; //multiplying then dividing by 100 is to round to 2 dp
+    let cost_per_person:number = Math.round(TotalBill * 100 / people) / 100; //multiplying then dividing by 100 is to round to 2 dp
     console.log(`\nTotal bill including tip is ${TotalBill}\nTotal cost per person is ${cost_per_person}`);
 }
