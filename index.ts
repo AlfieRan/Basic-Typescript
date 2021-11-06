a0d6263606b91b12cbcdd0c600ea745df7f52206
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-11-06 13:23:21 +0000

    ..

--- index.ts
+++ index.ts
@@ -16,6 +16,18 @@ import { LargerTesting, Test_HCF } from './Tasks/Functions&testing';
 import { PrintColours,SimonSays } from './Tasks/Lists';
 import { RunTask } from './Tasks/Bank Account Task/main';
 
+
+//// making this soon doesnt do anything yet
+class Task{
+    TaskName: string;
+    MainTask: string;
+    Run: Boolean = false;
+    Summary: string;
+    constructor(TaskName:string, MainTask:string, Summary:string){
+        console.log("Poggers");
+    }
+}
+
 // Depending on which of these are true is which ones run/don't run
 const Tasks: Array<VoidFunction> = [circleRadius, Sentance, FaceBookDataCollection, ChatBot, BillSplitter, GuessAnimal, GuessTheVeg, TimesTable, BetterTimesTable, NumberGuessing, OOPTask1, runPogShape, LargerTesting, Test_HCF, PrintColours, SimonSays, RunTask];
 const TaskName: Array<string> = ["Using Pi to find a radius Task", "random sentence task", "Task: collect user data", "Task: bad chatbot", "Bill splitter", "Animal Guessing game", "vegetable guessing game", "times table program", "user input times tables", "Number Guessing Game", "OOP Task 1 (Jeffery Bezzos)", "shapey time", "Which is larger (testing)", "Highest common factor (testing)", "Printing Colours of the Rainbow", "Simon Says Program", "Banking program"]
